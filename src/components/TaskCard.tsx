3e4c9038c963856abf1f0538a145e8cdeab6b940
JenniferSieber <sieberjen6@gmail.com>
2025-01-22 15:22:40 -0700

    Update Task Points and style

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,9 +1,9 @@
-// import from App the Type definitions for Task
+import { useState } from "react"
 import { Task } from "../utils/data-tasks"
-const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
 </svg>
-const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
 </svg>
@@ -11,10 +11,14 @@ const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
 </svg>
 
-// updated the task prop to look at task: Task
-const TaskCard = ({task}: {
+
+const TaskCard = ({task, updateTaskPoints}: {
   task: Task
+  updateTaskPoints: (task: Task, points: number) => void
 }) => {
+  // const [points, setPoints] = useState(task.points || 0)
+  const points = task.points || 0
+ 
   
   return (
     <div className="border rounded-lg px-2 m-2 bg-gray-50 w-60">
@@ -22,21 +26,18 @@ const TaskCard = ({task}: {
         {task.title}
       </div>
       <div className="flex text-sm justify-between py-2">
-        <div className="flex gap-3">
+        <div className="flex gap-2">
           <span>{task.id}</span>
-          {/* Add Validation Logic */}
             {task.priority === "high" ? <span className="flex text-center capitalize">{highPriorityIcon}</span> : null}
             {task.priority === "medium" ? <span className="text-center capitalize">{mediumPriorityIcon}</span> : null}
             {task.priority === "low" ? <span className="text-center capitalize">{lowPriorityIcon}</span> : null}
-            {/* {task.priority === "high" ? <span className="text-center capitalize">{task.priority}⬆️</span> : null}
-            {task.priority === "medium" ? <span className="text-center capitalize">{task.priority}⏸️</span> : null}
-            {task.priority === "low" ? <span className="text-center capitalize">{task.priority}⬇️</span> : null} */}
-            {/* {task.priority === "high" && <span>{task.priority}⬆️</span>}
-            {task.priority === "medium" && <span>{task.priority}⏸️</span>}
-            {task.priority === "low" && <span>{task.priority}⬇️</span>} */}
         </div>
-        
-        <div>{task.points}</div>
+        <div className="flex gap-2 items-center">
+          
+          <button onClick={() => updateTaskPoints(task, points-1)}>-</button>
+            <span className="font-bold ">{points}</span>
+          <button onClick={() => updateTaskPoints(task, points+1)}>+</button>
+        </div>
       </div>
     </div>
   );
