08b4a280e88622d251874a5dffd5be60100a359c
JenniferSieber <sieberjen6@gmail.com>
2025-01-22 11:06:23 -0700

    basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const title = "Do Market Research"
+  const id = "BUS-1"
+  const points = 5
   return (
-    <>
-      <div>
-        <h2>Hello Kanban Board 01</h2>
+    <div className="border rounded-lg px-2 m-2 bg-gray-50">
+
+      <div className="text-base font-semibold text-gray-700 py-2">
+        {title}
       </div>
-    </>
+      <div className="flex justify-between py-2">
+        <div>
+          {id}
+        </div>
+        <div>Middle Element</div>
+        <div>
+          {points}
+        </div>
+      </div>
+    </div>
   )
+  
 }
 
-export default App
+export default App;
